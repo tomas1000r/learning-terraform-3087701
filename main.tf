3820e8cb8ba3e58b1195e4e6a80c71df451a8a9b
Tomas Jakubco <tomas.jakubco@gmail.com>
2024-11-26 08:53:20 +0100

    Enable NAT gateway

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-# data "aws_ami" "app_ami" {
-#   most_recent = true
-#
-#   filter {
-#     name   = "name"
-#     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-#   }
-#
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-#
-#   owners = ["979382823631"] # Bitnami
-# }
-#
-# resource "aws_instance" "web" {
-#   ami           = data.aws_ami.app_ami.id
-#   instance_type = "t3.nano"
-#
-#   tags = {
-#     Name = "HelloWorld"
-#   }
-# }
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -33,7 +8,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
   enable_vpn_gateway = false
 
   tags = {
