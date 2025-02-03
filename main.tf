25bdeb0e5245e386f1146dc9f25b4d450a6c1432
Tomas Jakubco <tomas.jakubco@gmail.com>
2025-02-03 12:07:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_cognito_user_pool_client" "client" {
 
   user_pool_id = aws_cognito_user_pool.pool.id
 
-  explicit_auth_flows = ["ALLOW_USER_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH"]
+  explicit_auth_flows = ["ALLOW_USER_AUTH", "ALLOW_REFRESH_TOKEN_AUTH",  "ALLOW_USER_PASSWORD_AUTH"]
 
   generate_secret     = true
 }
