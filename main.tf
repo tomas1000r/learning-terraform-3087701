eb4dd1a49ab6edade7dfbf542e85d211efb9f9af
Tomas Jakubco <tomas.jakubco@gmail.com>
2025-02-17 15:01:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_cognito_user_pool" "pool" {
 }
 
 resource "aws_cognito_user_pool_client" "client" {
-  name = "client"
+  name = "client1"
 
   user_pool_id = aws_cognito_user_pool.pool.id
 
